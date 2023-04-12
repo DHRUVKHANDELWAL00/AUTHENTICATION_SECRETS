aad93a4a55563da17b6b04c80a7868e39c08610e
DHRUV KHANDELWAL <99539203+DHRUVKHANDELWAL00@users.noreply.github.com>
2023-04-12 09:28:28 +0530

    MAIN FILES ADDED

---
+++ app.js
@@ -0,0 +1,98 @@
+//jshint esversion:6
+require('dotenv').config()
+const express=require('express')
+const bodyParser=require('body-parser');
+const ejs=require('ejs')
+const mongoose=require('mongoose')
+const encrypt=require('mongoose-encryption')
+
+const app=express();
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+
+
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
+
+// encrypt age regardless of any other options. name and _id will be left unencrypted
+
+var userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+    // whatever else
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']  });
+
+const User=new mongoose.model("User",userSchema)
+
+
+app.get("/",function(req,res){
+  res.render("home")
+})
+app.get("/register",function(req,res){
+  res.render("register")
+})
+app.get("/login",function(req,res){
+  res.render("login")
+})
+
+
+
+app.post("/register",function(req,res){
+  const newUser=new User({
+    email: req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  })
+})
+
+
+app.post("/login",function(req,res){
+  const username= req.body.username;
+  const userpassword=req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password===userpassword){
+          res.render("secrets");
+        }
+      }
+    }
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("listening on port 3000...");
+})
